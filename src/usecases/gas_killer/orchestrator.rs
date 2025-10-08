6f0d67d99929744b0a2761efa4c9b22792affb3f
bagelface <bagelface@protonmail.com>
2025-10-08 13:55:59 -0400

    refactor: add dead_code attribute to orchestrator since it's not being used yet

--- src/usecases/gas_killer/orchestrator.rs
+++ src/usecases/gas_killer/orchestrator.rs
@@ -3,6 +3,7 @@ use crate::orchestrator::generic::Orchestrator;
 use crate::usecases::gas_killer::{GasKillerCreatorType, GasKillerHandler, GasKillerValidator};
 
 /// Type alias for counter orchestrator using the generic framework
+#[allow(dead_code)]
 pub type GasKillerOrchestrator<C> = Orchestrator<
     GasKillerCreatorType,
     BlsEigenlayerExecutor<GasKillerHandler>,
