7274ff9718749d927a58976409236bf26751965a
bagelface <bagelface@protonmail.com>
2026-03-23 17:12:21 -0400

    refactor: run formatter

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -5,8 +5,8 @@ pub mod validator;
 
 // Re-export commonly used types
 pub use config::{
-    ChainId, KeyConfig, OrchestratorConfig, detect_chain_for_address,
-    get_operator_states, load_key_from_file, load_orchestrator_config,
+    ChainId, KeyConfig, OrchestratorConfig, detect_chain_for_address, get_operator_states,
+    load_key_from_file, load_orchestrator_config,
 };
 pub use task_data::GasKillerTaskData;
 pub use validator::GasKillerValidator;
